b9dd9a8870cf74bd75e93a9ecc0d6c35fc478e11
Peter <petercort@github.com>
2024-09-27 14:28:43 -0400

    Update wait-for-approval.js

--- .github/scripts/snow/wait-for-approval.js
+++ .github/scripts/snow/wait-for-approval.js
@@ -1,7 +1,6 @@
-const axios = require('axios')
-const core = require('@actions/core');
+import axios from 'axios';
 
-module.exports = async function waitForApproval(){
+module.exports = async function waitForApproval(core) {
   const url = `${process.env.SNOW_URL}/api/sn_chg_rest/change/normal/${process.env.sys_id}`
   const username = process.env.SNOW_USERNAME
   const password = process.env.SNOW_PASSWORD
@@ -11,10 +10,11 @@ module.exports = async function waitForApproval(){
   }
   try {
     var response = await axios(url, { auth })
-    while (response && response.data.result.state.value !== '3') {
+    console.log(response.data)
+    while (response.data.result.state.value !== '3') {
       console.log(`Request state: ${response.data.result.state.display_value}`)
       await new Promise(r => setTimeout(r, 5000));
-      response = await axios.get(url, { auth })
+      response = await axios(url, { auth })
     }
     core.setOutput('state', response.data.result.state.approval_history.display_value);
   } catch (error) {
