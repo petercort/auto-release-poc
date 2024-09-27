bcbef85fe85a38030029799a433c152f02009d9d
Peter <petercort@github.com>
2024-09-27 14:28:33 -0400

    Update create-change-request.js

--- .github/scripts/snow/create-change-request.js
+++ .github/scripts/snow/create-change-request.js
@@ -1,8 +1,7 @@
 const axios = require('axios')
-const core = require('@actions/core');
 
-module.exports = async function createChangeRequest(){
-  const url = `${process.env.SNOW_URL}/api/sn_chg_rest/change/normal`
+module.exports = async function createChangeRequest(core){
+  const url = `${process.env.SNOWURL}/api/sn_chg_rest/change/normal`
   const username = process.env.SNOW_USERNAME
   const password = process.env.SNOW_PASSWORD
   const auth = {
@@ -16,9 +15,9 @@ module.exports = async function createChangeRequest(){
     "requested_by": "requested_by_user_sys_id"
   }
   try {
-    const response = await axios.post(url, body, { auth })
-    console.log(`Created request: ${response.data.result.sys_id.value}`)
-    core.setOutput('sys_id', response.data.result.sys_id.value);
+  const response = await axios.post(url, body, { auth })
+  console.log(`Created request: ${response.data.number.display_value}`)
+  core.setOutput('sys_id', response.data.number.sys_id.value);
   } catch (error) {
     core.setFailed(error.message);
   }
